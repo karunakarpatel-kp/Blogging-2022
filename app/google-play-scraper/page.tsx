5b9eefdd20dd1b57e6dff8a66200da5525865710
Karunakar_Patel <karunakarpatel.kp@gmail.com>
2025-08-18 17:02:17 +0530

    Added first API

---
+++ app/google-play-scraper/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const GooglePlayScraper = () => {
+  return (
+    <div className="border-0 border-red-800 mt-20 pl-5 pr-3">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, neque numquam. Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Adipisci, neque numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Adipisci, neque numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, neque numquam. Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Adipisci, neque numquam.
+    </div>
+  );
+};
+
+export default GooglePlayScraper;
